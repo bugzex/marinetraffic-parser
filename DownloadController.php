442afd37c3dfa8caf74a39d8e79f35be04b9b4bc
maksim-trofimov <maksim.trofimov@vvsu.ru>
2016-02-18 09:08:40 +1000

    форматирование psr 1/2

--- DownloadController.php
+++ DownloadController.php
@@ -4,13 +4,13 @@
  * разработана в академических целях, использовать на свой страх и риск
  *
  * @link https://github.com/maksim-trofimov/marinetraffic-parser
- * 
+ *
  * @copyright Maksim Trofimov <maksim.trofimov@vvsu.ru>
- * 
+ *
  * @author Viktor Grinyak <Viktor.Grinyak@vvsu.ru>
  * @author Maksim Trofimov <maksim.trofimov@vvsu.ru>
  * @author Boris Golovchenko <boris.golovchenko@vvsu.ru>
- * 
+ *
  * @license The MIT License (MIT)
  */
 
@@ -23,9 +23,9 @@ use yii\console\Controller;
 
 // todo: разместить библиотеки в vendor
 set_include_path(
-	get_include_path().PATH_SEPARATOR .
-	dirname(__FILE__).'/lib'.PATH_SEPARATOR .
-	dirname(__FILE__).'/lib/HTTP'
+    get_include_path() . PATH_SEPARATOR .
+    dirname(__FILE__) . '/lib' . PATH_SEPARATOR .
+    dirname(__FILE__) . '/lib/HTTP'
 );
 
 // todo: библиотека устарела
@@ -37,156 +37,161 @@ require_once 'Request.php';
  */
 class DownloadController extends Controller
 {
-    public function actionIndex($url = null, $sw_x = 130.0, $sw_y = 43.0, $ne_x = 133.0, $ne_y = 43.5, $zoom = 12, $time_zone = 'Asia/Vladivostok', $sleep_seconds = 10)
-    {	
-		// устанавливаем временную зону по-умолчанию
-		date_default_timezone_set($time_zone);
+    public function actionIndex(
+        $url = null,
+        $sw_x = 130.0,
+        $sw_y = 43.0,
+        $ne_x = 133.0,
+        $ne_y = 43.5,
+        $zoom = 12,
+        $time_zone = 'Asia/Vladivostok',
+        $sleep_seconds = 10
+    ) {
+        // устанавливаем временную зону по-умолчанию
+        date_default_timezone_set($time_zone);
 
-		// -- устанавливаем кодировку, чтобы в консоли отображалась кирилица корректно (для windows)
+        // -- устанавливаем кодировку, чтобы в консоли отображалась кирилица корректно (для windows)
         ob_start('ob_iconv_handler');
 
-		$options = [
-			'iconv.input_encoding'    => 'cp866',
-			'iconv.output_encoding'   => 'cp866',
-			'iconv.internal_encoding' => 'UTF-8',
-		];
+        $options = [
+            'iconv.input_encoding' => 'cp866',
+            'iconv.output_encoding' => 'cp866',
+            'iconv.internal_encoding' => 'UTF-8',
+        ];
 
-		foreach ($options as $k => $v) {
-			ini_set($k, $v);
-		}
-		// -- -- --
+        foreach ($options as $k => $v) {
+            ini_set($k, $v);
+        }
+        // -- -- --
 
-		// параметры http-запроса
-		$request_params = array('method' => 'GET');
-		
-		// этим запросом получаем индентификатор сессии, который будем использовать при последующих запросах к сервису
-		// url ссылается на страницу входа, но, по сути, можно использовать любую другую
-		// в сервисе в url содержатся двоеточия - это разделитель ключа и значения параметра
-		$url = 'http://www.marinetraffic.com/ru/users/ajax_user_menu/home:1';
+        // параметры http-запроса
+        $request_params = array('method' => 'GET');
 
-		$requrest = new \HTTP_Request($url, $request_params);
+        // этим запросом получаем индентификатор сессии, который будем использовать при последующих запросах к сервису
+        // url ссылается на страницу входа, но, по сути, можно использовать любую другую
+        // в сервисе в url содержатся двоеточия - это разделитель ключа и значения параметра
+        $url = 'http://www.marinetraffic.com/ru/users/ajax_user_menu/home:1';
 
-		// использование прокси: http://pear.php.net/manual/ru/package.http.http-request.proxy-auth.php
-		//$requrest->setProxy('proxy.example.com', 8080, 'johndoe', 'foo');
+        $requrest = new \HTTP_Request($url, $request_params);
 
-		// проверка работы скрипта через прокси из списка: http://proxylist.hidemyass.com/2
-		//$requrest->setProxy('217.175.34.170', 8080);
+        // использование прокси: http://pear.php.net/manual/ru/package.http.http-request.proxy-auth.php
+        //$requrest->setProxy('proxy.example.com', 8080, 'johndoe', 'foo');
 
-		$requrest->sendRequest();
-		$requrest->disconnect();
-		$cookies = $requrest->getResponseCookies();
-		$session_id = $cookies[0]['value'];
+        // проверка работы скрипта через прокси из списка: http://proxylist.hidemyass.com/2
+        //$requrest->setProxy('217.175.34.170', 8080);
 
-		// бесконечный цикл для сбора данных
-		$url = "http://www.marinetraffic.com/map/getDataJson/sw_x:{$sw_x}/sw_y:{$sw_y}/ne_x:{$ne_x}/ne_y:{$ne_y}/zoom:{$zoom}/fleet:/station:0";
-		while (true) {
-			// -- делаем запрос на получение данных
-			$requrest = new \HTTP_Request($url, $request_params);
-			$requrest->addHeader('Referer', $url);
-			$requrest->addCookie('CAKEPHP', $session_id);
-			$requrest->sendRequest();
-			$requrest->disconnect();
+        $requrest->sendRequest();
+        $requrest->disconnect();
+        $cookies = $requrest->getResponseCookies();
+        $session_id = $cookies[0]['value'];
+
+        // бесконечный цикл для сбора данных
+        $url = "http://www.marinetraffic.com/map/getDataJson/sw_x:{$sw_x}/sw_y:{$sw_y}/ne_x:{$ne_x}/ne_y:{$ne_y}/zoom:{$zoom}/fleet:/station:0";
+        while (true) {
+            // -- делаем запрос на получение данных
+            $requrest = new \HTTP_Request($url, $request_params);
+            $requrest->addHeader('Referer', $url);
+            $requrest->addCookie('CAKEPHP', $session_id);
+            $requrest->sendRequest();
+            $requrest->disconnect();
             // -- -- --
-			
-			// -- пропускаем итерацию при недолжном ответе
-			if ($requrest->getResponseCode() !== 200 || empty($requrest->getResponseBody())) {
+
+            // -- пропускаем итерацию при недолжном ответе
+            if ($requrest->getResponseCode() !== 200 || empty($requrest->getResponseBody())) {
                 echo 'Сервер сервиса вернул ошибку или пустые данные (возможно сервис недоступен).' . PHP_EOL;
                 echo 'Пробую повторно.' . PHP_EOL;
-				continue;
+                continue;
             }
             // -- -- --
 
-			// -- декодируем json-объект в массив
-			$decodeResult = json_decode($requrest->getResponseBody());
-			if (($decodeResult instanceof \stdClass) === false) {
+            // -- декодируем json-объект в массив
+            $decodeResult = json_decode($requrest->getResponseBody());
+            if (($decodeResult instanceof \stdClass) === false) {
                 echo 'Декодированные данные оказались другого типа (нужно менять код программы).' . PHP_EOL;
                 echo 'Выхожу.' . PHP_EOL;
-				return;
-            }
-            elseif (!isset($decodeResult->data->rows) || !is_array($decodeResult->data->rows)) {
+                return;
+            } elseif (!isset($decodeResult->data->rows) || !is_array($decodeResult->data->rows)) {
                 echo 'Декодированные данные о суднах и их движении имеют другой тип или изменился формат данных (нужно менять код программы).' . PHP_EOL;
                 echo 'Выхожу.' . PHP_EOL;
                 return;
-            }
-            else {
+            } else {
                 echo 'Всего будет обработано ' . count($decodeResult->data->rows) . ' записей.' . PHP_EOL;
             }
             // -- -- --
 
-			// перебираем массив и сохраняем данные в базу
-			foreach ($decodeResult->data->rows as $marineData) { /** @var \StdClass $marineData */
-				// -- преобразуем атрибуты к нижнему регистру
+            // перебираем массив и сохраняем данные в базу
+            foreach ($decodeResult->data->rows as $marineData) {
+                /** @var \StdClass $marineData */
+                // -- преобразуем атрибуты к нижнему регистру
                 $data = [];
-				foreach ($marineData as $attributeName => $attributeValue) {
-					$data[strtolower($attributeName)] = $attributeValue;
-				}
-				// -- -- --
+                foreach ($marineData as $attributeName => $attributeValue) {
+                    $data[strtolower($attributeName)] = $attributeValue;
+                }
+                // -- -- --
 
-				// -- заполним прокси-объект
-				$marinetrafficResult = new MarinetrafficResult();
-				$marinetrafficResult->setAttributes($data);
+                // -- заполним прокси-объект
+                $marinetrafficResult = new MarinetrafficResult();
+                $marinetrafficResult->setAttributes($data);
                 if ($marinetrafficResult->validate() === false) {
                     echo 'Ошибка при загрузки в прокси-объект данных, полученных из запроса: возможно изменился формат данных.' . PHP_EOL;
                     echo 'Выхожу.' . PHP_EOL;
                     return;
                 }
                 // -- -- --
-				
-				// идентифицируем судно по имени, т.к. идентификатор mmsi больше не передается, а идентификатор судна по
+
+                // идентифицируем судно по имени, т.к. идентификатор mmsi больше не передается, а идентификатор судна по
                 // сервису не всегда совпадает (вероятно их внутренний идентификатор, который иногда меняется)
-				$marine = Marine::findOne(['name' => strtoupper($marinetrafficResult->shipname)]);
+                $marine = Marine::findOne(['name' => strtoupper($marinetrafficResult->shipname)]);
 
-				// -- если судно отсутствует в базе, то добавляем его
-				if ($marine === null) {
-					$marine = new Marine();
-					$marine->identifier = $marinetrafficResult->ship_id;
-					$marine->name   = $marinetrafficResult->shipname;
-					$marine->type   = $marinetrafficResult->shiptype;
-					$marine->flag   = $marinetrafficResult->flag;
-					$marine->length = $marinetrafficResult->length;
-					$marine->col11  = $marinetrafficResult->width;
-					$marine->port   = $marinetrafficResult->destination;
-					$marine->col12  = $marinetrafficResult->l_fore;
-					$marine->col13  = $marinetrafficResult->w_left;
-					$marine->col15  = $marinetrafficResult->rot;
+                // -- если судно отсутствует в базе, то добавляем его
+                if ($marine === null) {
+                    $marine = new Marine();
+                    $marine->identifier = $marinetrafficResult->ship_id;
+                    $marine->name = $marinetrafficResult->shipname;
+                    $marine->type = $marinetrafficResult->shiptype;
+                    $marine->flag = $marinetrafficResult->flag;
+                    $marine->length = $marinetrafficResult->length;
+                    $marine->col11 = $marinetrafficResult->width;
+                    $marine->port = $marinetrafficResult->destination;
+                    $marine->col12 = $marinetrafficResult->l_fore;
+                    $marine->col13 = $marinetrafficResult->w_left;
+                    $marine->col15 = $marinetrafficResult->rot;
 
-					if ($marine->save()) {
-						echo 'Добавлено новое судно: ' . $marine->name . PHP_EOL;
-					}
-					else {
-						echo 'Неудалось добавить новое судно из-за ошибок:' . PHP_EOL ;
-						echo print_r($marine->getErrors(), true) . PHP_EOL;
-					}
-				}
-                // -- -- --
-
-				// -- сохраняем положение судна, если не было ошибок
-				if ($marine->id_marine !== null) {
-					$track = new Track();
-					$track->id_marine = $marine->id_marine;
-					$track->lat       = $marinetrafficResult->lat;
-					$track->lon       = $marinetrafficResult->lon;
-					$track->course    = $marinetrafficResult->course;
-                    //$track->course    = $marinetrafficResult->heading; // (heading похож на course)
-					$track->speed     = $marinetrafficResult->speed;
-                    $track->age       = $marinetrafficResult->elapsed;
-					$track->date_add  = date('Y-m-d_H:i');
-
-					if ($track->save()) {
-						echo 'Добавлено положение судна: ' . $marine->name . PHP_EOL;
+                    if ($marine->save()) {
+                        echo 'Добавлено новое судно: ' . $marine->name . PHP_EOL;
+                    } else {
+                        echo 'Неудалось добавить новое судно из-за ошибок:' . PHP_EOL;
+                        echo print_r($marine->getErrors(), true) . PHP_EOL;
                     }
-					else {
-						echo 'Неудалось добавить положение судна из-за ошибок: ' . PHP_EOL;
-                        echo print_r($track->getErrors(), true) . PHP_EOL;
-					}
-				}
+                }
                 // -- -- --
-			}
+
+                // -- сохраняем положение судна, если не было ошибок
+                if ($marine->id_marine !== null) {
+                    $track = new Track();
+                    $track->id_marine = $marine->id_marine;
+                    $track->lat = $marinetrafficResult->lat;
+                    $track->lon = $marinetrafficResult->lon;
+                    $track->course = $marinetrafficResult->course;
+                    //$track->course    = $marinetrafficResult->heading; // (heading похож на course)
+                    $track->speed = $marinetrafficResult->speed;
+                    $track->age = $marinetrafficResult->elapsed;
+                    $track->date_add = date('Y-m-d_H:i');
+
+                    if ($track->save()) {
+                        echo 'Добавлено положение судна: ' . $marine->name . PHP_EOL;
+                    } else {
+                        echo 'Неудалось добавить положение судна из-за ошибок: ' . PHP_EOL;
+                        echo print_r($track->getErrors(), true) . PHP_EOL;
+                    }
+                }
+                // -- -- --
+            }
 
             // выталкиваем сообщения в стандартный вывод и делаем паузу
             echo 'Жду ' . $sleep_seconds . ' секунд...' . PHP_EOL;
             ob_flush();
-			sleep($sleep_seconds);
-		}
+            sleep($sleep_seconds);
+        }
     }
 }
