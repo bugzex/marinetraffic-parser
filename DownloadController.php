57cf457a441264fc84f298735b2c6819e17860de
maksim-trofimov <maksim.trofimov@vvsu.ru>
2016-02-18 09:04:24 +1000

    изменения в следствии изменения формата данных севиса; различные улучшения

--- DownloadController.php
+++ DownloadController.php
@@ -16,13 +16,16 @@
 
 namespace app\commands;
 
+use app\models\Marine;
+use app\models\MarinetrafficResult;
+use app\models\Track;
 use yii\console\Controller;
 
 // todo: разместить библиотеки в vendor
 set_include_path(
-		get_include_path().PATH_SEPARATOR.
-		dirname(__FILE__).'/lib'.PATH_SEPARATOR.
-		dirname(__FILE__).'/lib/HTTP'
+	get_include_path().PATH_SEPARATOR .
+	dirname(__FILE__).'/lib'.PATH_SEPARATOR .
+	dirname(__FILE__).'/lib/HTTP'
 );
 
 // todo: библиотека устарела
@@ -30,42 +33,28 @@ require_once 'Request.php';
 
 /**
  * контроллер загрузки данных
- * @todo проанализировать результаты - что-то не то со скоростью и пр.
+ * @todo сопоставить поля, если неверно назначены
  */
 class DownloadController extends Controller
 {
-	/**
-	 * описание json-объекта, возвращаемого сервисом
-	 * если формат изменится сервисом, то нужно будет отредактировать это описание и описание моделей классов
-	 * 
-	 * @var array
-	 */
-	private static $json_map = array(
-		0 => 'identifier',		// 345901
-		1 => 'lon',				// 43.08045
-		2 => 'lat',				// 132.3173
-		3 => 'name',			// GARMONIYA
-		4 => 'type',			// 7
-		5 => 'course',			// 511
-		6 => 'speed',			// 0
-		7 => 'flag',			// RU
-		8 => 'mmsi',			// 273312860,
-		9 => 'length',			// 103
-		10 => 'age',			// 21
-		11 => 'col11',			// 18
-		12 => 'col12',			// 0
-		13 => 'col13',			// 0
-		14 => 'col14',			// 0
-		15 => 'col15',			// 0
-		16 => 'port',			// YUZH LIFLYANDIYA
-        17 => 'col17',          // неизвестное поле: целое число
-        18 => 'col18',          // неизвестное поле: целое число
-	);
-	
     public function actionIndex($url = null, $sw_x = 130.0, $sw_y = 43.0, $ne_x = 133.0, $ne_y = 43.5, $zoom = 12, $time_zone = 'Asia/Vladivostok', $sleep_seconds = 10)
     {	
 		// устанавливаем временную зону по-умолчанию
 		date_default_timezone_set($time_zone);
+
+		// -- устанавливаем кодировку, чтобы в консоли отображалась кирилица корректно (для windows)
+        ob_start('ob_iconv_handler');
+
+		$options = [
+			'iconv.input_encoding'    => 'cp866',
+			'iconv.output_encoding'   => 'cp866',
+			'iconv.internal_encoding' => 'UTF-8',
+		];
+
+		foreach ($options as $k => $v) {
+			ini_set($k, $v);
+		}
+		// -- -- --
 
 		// параметры http-запроса
 		$request_params = array('method' => 'GET');
@@ -89,69 +78,115 @@ class DownloadController extends Controller
 		$session_id = $cookies[0]['value'];
 
 		// бесконечный цикл для сбора данных
-		$url = "http://www.marinetraffic.com/map/getjson/sw_x:{$sw_x}/sw_y:{$sw_y}/ne_x:{$ne_x}/ne_y:{$ne_y}/zoom:{$zoom}/fleet:/station:0";
+		$url = "http://www.marinetraffic.com/map/getDataJson/sw_x:{$sw_x}/sw_y:{$sw_y}/ne_x:{$ne_x}/ne_y:{$ne_y}/zoom:{$zoom}/fleet:/station:0";
 		while (true) {
-			// делаем запрос на получение данных
+			// -- делаем запрос на получение данных
 			$requrest = new \HTTP_Request($url, $request_params);
 			$requrest->addHeader('Referer', $url);
 			$requrest->addCookie('CAKEPHP', $session_id);
 			$requrest->sendRequest();
 			$requrest->disconnect();
+            // -- -- --
 			
-			// пропускаем итерацию при недолжном ответе
-			if ($requrest->getResponseCode() !== 200 || empty($requrest->getResponseBody()))
+			// -- пропускаем итерацию при недолжном ответе
+			if ($requrest->getResponseCode() !== 200 || empty($requrest->getResponseBody())) {
+                echo 'Сервер сервиса вернул ошибку или пустые данные (возможно сервис недоступен).' . PHP_EOL;
+                echo 'Пробую повторно.' . PHP_EOL;
 				continue;
-			
-			// декодируем json-объект в массив
-			$data = json_decode($requrest->getResponseBody());
-			if (!is_array($data))
-				continue;
-			
+            }
+            // -- -- --
+
+			// -- декодируем json-объект в массив
+			$decodeResult = json_decode($requrest->getResponseBody());
+			if (($decodeResult instanceof \stdClass) === false) {
+                echo 'Декодированные данные оказались другого типа (нужно менять код программы).' . PHP_EOL;
+                echo 'Выхожу.' . PHP_EOL;
+				return;
+            }
+            elseif (!isset($decodeResult->data->rows) || !is_array($decodeResult->data->rows)) {
+                echo 'Декодированные данные о суднах и их движении имеют другой тип или изменился формат данных (нужно менять код программы).' . PHP_EOL;
+                echo 'Выхожу.' . PHP_EOL;
+                return;
+            }
+            else {
+                echo 'Всего будет обработано ' . count($decodeResult->data->rows) . ' записей.' . PHP_EOL;
+            }
+            // -- -- --
+
 			// перебираем массив и сохраняем данные в базу
-			foreach ($data as $marine_data) {
-				// в ответ могут быть включены не только данные о движениях судна - они нас не интересуют
-				if (count($marine_data) != count(self::$json_map))
-					continue;
+			foreach ($decodeResult->data->rows as $marineData) { /** @var \StdClass $marineData */
+				// -- преобразуем атрибуты к нижнему регистру
+                $data = [];
+				foreach ($marineData as $attributeName => $attributeValue) {
+					$data[strtolower($attributeName)] = $attributeValue;
+				}
+				// -- -- --
+
+				// -- заполним прокси-объект
+				$marinetrafficResult = new MarinetrafficResult();
+				$marinetrafficResult->setAttributes($data);
+                if ($marinetrafficResult->validate() === false) {
+                    echo 'Ошибка при загрузки в прокси-объект данных, полученных из запроса: возможно изменился формат данных.' . PHP_EOL;
+                    echo 'Выхожу.' . PHP_EOL;
+                    return;
+                }
+                // -- -- --
 				
-				// получим ассоциативный массив данных
-				$data = array_combine(array_values(self::$json_map), $marine_data);
-				
-				// идентифицируем судно
-				$marine = \app\models\Marine::findOne(['mmsi' => $data['mmsi']]);
-				
-				// если судно отсутствует в базе, то добавляем его
-				if (!$marine) {
-					$marine = new \app\models\Marine();
-					$marine->setAttributes($data);
-					if ($marine->save(false)) {
-						echo 'M';
+				// идентифицируем судно по имени, т.к. идентификатор mmsi больше не передается, а идентификатор судна по
+                // сервису не всегда совпадает (вероятно их внутренний идентификатор, который иногда меняется)
+				$marine = Marine::findOne(['name' => strtoupper($marinetrafficResult->shipname)]);
+
+				// -- если судно отсутствует в базе, то добавляем его
+				if ($marine === null) {
+					$marine = new Marine();
+					$marine->identifier = $marinetrafficResult->ship_id;
+					$marine->name   = $marinetrafficResult->shipname;
+					$marine->type   = $marinetrafficResult->shiptype;
+					$marine->flag   = $marinetrafficResult->flag;
+					$marine->length = $marinetrafficResult->length;
+					$marine->col11  = $marinetrafficResult->width;
+					$marine->port   = $marinetrafficResult->destination;
+					$marine->col12  = $marinetrafficResult->l_fore;
+					$marine->col13  = $marinetrafficResult->w_left;
+					$marine->col15  = $marinetrafficResult->rot;
+
+					if ($marine->save()) {
+						echo 'Добавлено новое судно: ' . $marine->name . PHP_EOL;
 					}
 					else {
-						echo 'X';
-						// todo: не выводить на консоль, а сохранять в лог сообщение об ошибке
-						print_r($marine->getErrors());
+						echo 'Неудалось добавить новое судно из-за ошибок:' . PHP_EOL ;
+						echo print_r($marine->getErrors(), true) . PHP_EOL;
 					}
 				}
-				
-				// сохраняем положение судна
-				if ($marine->id_marine)
-				{
-					$track = new \app\models\Track();
-					$track->setAttributes($data);
+                // -- -- --
+
+				// -- сохраняем положение судна, если не было ошибок
+				if ($marine->id_marine !== null) {
+					$track = new Track();
 					$track->id_marine = $marine->id_marine;
-					$track->date_add = date('Y-m-d_H:i');
-					if ($track->save(false))
-						echo 'T';
+					$track->lat       = $marinetrafficResult->lat;
+					$track->lon       = $marinetrafficResult->lon;
+					$track->course    = $marinetrafficResult->course;
+                    //$track->course    = $marinetrafficResult->heading; // (heading похож на course)
+					$track->speed     = $marinetrafficResult->speed;
+                    $track->age       = $marinetrafficResult->elapsed;
+					$track->date_add  = date('Y-m-d_H:i');
+
+					if ($track->save()) {
+						echo 'Добавлено положение судна: ' . $marine->name . PHP_EOL;
+                    }
 					else {
-						echo 'X';							
-						// todo: не выводить на консоль, а сохранять в лог сообщение об ошибке
-						print_r($track->getErrors());
+						echo 'Неудалось добавить положение судна из-за ошибок: ' . PHP_EOL;
+                        echo print_r($track->getErrors(), true) . PHP_EOL;
 					}
 				}
+                // -- -- --
 			}
-			
-			// пауза между запросами
+
+            // выталкиваем сообщения в стандартный вывод и делаем паузу
+            echo 'Жду ' . $sleep_seconds . ' секунд...' . PHP_EOL;
+            ob_flush();
 			sleep($sleep_seconds);
-		}		
+		}
     }
 }
